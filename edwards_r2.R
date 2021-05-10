eca1bd7b7d1846c7d200850c7070c598e7c10d33
joelleforestier <59805791+joelleforestier@users.noreply.github.com>
2021-05-10 11:28:30 -0400

    Add files via upload

---
+++ edwards_r2.R
@@ -0,0 +1,16 @@
+# calculate semipartial r2 using the method from Edwards et al., 2008
+# 'model' argument takes a lmerTest object
+# Joel Le Foresiter
+# joel.leforestier@mail.utoronto.ca
+# May 10, 2021
+
+edwards_r2 <- function(model) {
+  output <- anova(model, type = 3)
+  
+  for(p in 1:length(output$`F value`)) { # calculate R2 for each predictor in the model
+    cat(rownames(output)[p], 
+        " R2:",
+        round(((output$NumDF[p] / output$DenDF[p]) * output$`F value`[p]) / (1 + ((output$NumDF[p] / output$DenDF[p]) * output$`F value`[p])), 2), 
+        "\n")
+  }
+}
